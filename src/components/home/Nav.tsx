1083175e19c1b12a867d7bd973fbe81769a7589d
= <shawaarma0200@gmail.com>
2023-02-15 14:45:16 +0300

    finish the first phase of the homepage

--- src/components/home/Nav.tsx
+++ src/components/home/Nav.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 export default function hi() {
     return (
         <div className=' w-full  h-max    shadow-sm border-b border-x-gray-500 p-5 flex justify-between'>
@@ -14,11 +15,27 @@ export default function hi() {
 
                 }}
             />
-            <MenuIcon sx={{
-                color: "#6C5CE6",
-                fontSize: "30px"
+            <div className=' md:flex  justify-between gap-3  hidden'>
+                <button className=' w-max bg-brand-primary px-5 py-2 font-roboto  text-xl rounded text-brand-secondary'>Publish</button>
+                <button className=' text-brand-base font-roboto text-lg'>Log In</button>
+                <NotificationsNoneOutlinedIcon sx={{
+                    alignSelf: 'center',
+                    fontSize: "30px"
 
-            }} />
+                }} />
+
+
+            </div>
+            <div className=' md:hidden block sm:block '>
+                <MenuIcon sx={{
+                    color: "#6C5CE6",
+                    fontSize: "30px"
+
+
+                }} />
+            </div>
+
+
         </div>
     )
 }
---
+++ src/components/home/Sidebar.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import Image from 'next/image';
+export default function Sidebar() {
+    return (
+        <div className=' sm:hidden md:block lg:block xl:block hidden   w-max h-max  bg-white  ml-5 mt-5'>
+            <div className=' w-full h-max flex flex-col justify-center border border-brand-primary '>
+                <div className=' flex border-b-2 border-brand-base/10 p-3 '>
+                    <LibraryBooksOutlinedIcon sx={{
+                        color: "#6C5CE6"
+                    }} />
+                    <p className=' ml-2 font-roboto  text-brand-base'>Library</p>
+
+                </div>
+                <div className=' flex border-b-2 border-brand-base/20 p-3 '>
+                    <LibraryBooksOutlinedIcon sx={{
+                        color: "#6C5CE6"
+                    }} />
+                    <p className=' ml-2 font-roboto  text-brand-base'>Lists</p>
+
+                </div>
+                <div className=' flex border-b-2 border-brand-base/10 p-3 '>
+                    <PersonOutlineOutlinedIcon sx={{
+                        color: "#6C5CE6"
+                    }} />
+                    <p className=' ml-2 font-roboto  text-brand-base'>Account</p>
+
+                </div>
+                <div className=' flex border-b-2 border-brand-base/10 p-3 '>
+                    <SettingsOutlinedIcon sx={{
+                        color: "#6C5CE6"
+                    }} />
+                    <p className=' ml-2 font-roboto  text-brand-base '>Settings</p>
+
+                </div>
+
+            </div>
+
+            <div className=' w-full h-max flex flex-col justify-center border border-brand-primary mt-6 '>
+                <div className=' flex border-b-2 border-brand-base/10 p-3 bg-brand-primary  justify-center '>
+
+                    <p className='  font-roboto   text-brand-secondary'>Trending</p>
+
+
+
+
+                </div>
+                <div className=' flex border-b-2 border-brand-base/20 p-3 '>
+
+                    <i className="devicon-react-original  text-xl text-brand-base"></i>
+
+                    <p className=' ml-2 font-roboto '>React</p>
+
+                </div>
+                <div className=' flex border-b-2 border-brand-base/10 p-3 '>
+
+
+                    <i className="devicon-vuejs-plain-wordmark colored text-2xl"></i>
+
+
+                    <p className=' ml-2 font-roboto  text-brand-base'>Vue js</p>
+
+                </div>
+                <div className=' flex border-b-2 border-brand-base/10 p-3  '>
+
+                    <i className="devicon-nextjs-original colored text-2xl "></i>
+
+                    <p className=' ml-2 font-roboto  text-brand-base '>Nextjs</p>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
